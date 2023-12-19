ee30616676343492469ee44abc193b8f0653708b
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-12-18 23:31:22 -0400

    added invoices router

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.api.foods import foods as foods_router
+from app.api.invoices import inv as invoices_router
 
 app = FastAPI()
 
 # Routers
 app.include_router(foods_router, prefix="/food", tags=["foods"])
+app.include_router(invoices_router, prefix="/invoice", tags=["invoices"])
