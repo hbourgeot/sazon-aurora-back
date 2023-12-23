d304bd849554b12c5a55f8e0a21bd8ab53a03490
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-12-23 02:39:17 -0400

    adding roles.py at supa-functions and api

--- app/supabase/functions/foods.py
+++ app/supabase/functions/foods.py
@@ -4,14 +4,20 @@ from app.supabase import supabase
 def get_foods():
     res = supabase.from_("foods").select("*").execute()
     print(res)
+    return res
 
 
 def get_food_by_id(food_id: int):
     res = supabase.from_("foods").select("*").eq("id", food_id)
     print(res)
+    return res
 
 
 def upsert_food(data):
-    res = supabase.table("foods").upsert(data)
-    print(res)
+    try:
+        res = supabase.table("foods").upsert(data).execute()
+        print(res)
+        return res
+    except Exception as ex:
+        raise ex
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.api.foods import foods as foods_router
 from app.api.invoices import inv as invoices_router
 from app.api.products import products as products_router
 from app.api.providers import provs as providers_router
+from app.api.roles import roles as roles_router
 
 app = FastAPI()
 
@@ -11,3 +12,4 @@ app.include_router(foods_router, prefix="/food", tags=["foods"])
 app.include_router(invoices_router, prefix="/invoice", tags=["invoices"])
 app.include_router(products_router, prefix="/product", tags=["products"])
 app.include_router(providers_router, prefix="/provider", tags=["providers"])
+app.include_router(roles_router, prefix="/role", tags=["role"])
