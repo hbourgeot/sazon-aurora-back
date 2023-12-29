39a551acab9ab196eec4df84f413c3a3aa91ff08
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-12-28 20:47:04 -0400

    adding routes

--- app/supabase/functions/users.py
+++ app/supabase/functions/users.py
@@ -1,17 +1,19 @@
 from app.supabase import supabase
 
 
-def get_foods():
-    res = supabase.from_("foods").select("*").execute()
+def get_user():
+    res = supabase.from_("users").select("*").execute()
     print(res)
+    return res
 
 
-def get_food_by_id(food_id: int):
-    res = supabase.from_("foods").select("*").eq("id", food_id)
+def get_user_by_id(user_id: int):
+    res = supabase.from_("users").select("*").eq("id", user_id).execute()
     print(res)
+    return res
 
-
-def upsert_food(data):
-    res = supabase.table("foods").upsert(data)
+def upsert_user(data):
+    res = supabase.table("users").upsert(data).execute()
     print(res)
+    return res
 
