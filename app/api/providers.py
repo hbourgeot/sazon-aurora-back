3251a847aea20c263a9e2cac48ba1ef708e55066
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-01-20 00:48:52 -0400

    fixing endpoint and supafunctions

--- app/api/providers.py
+++ app/api/providers.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from app.schemas import Provider
+from app.supabase.functions.providers import upsert_provider, get_providers
 
 provs = APIRouter()
 
@@ -6,7 +8,9 @@ provs = APIRouter()
 # Routes
 @provs.get("/all")
 def get_provs():
-    return {"take": []}
+    res = get_providers()
+    data = res.model_dump()
+    return {"providers": data["data"]}
 
 
 @provs.get("/{id}")
@@ -15,8 +19,14 @@ def get_prov(id: int):
 
 
 @provs.post("/new")
-def create_prov():
-    return {"message": "Hey"}
+def create_prov(prov: Provider):
+
+    provider_dict = prov.model_dump()
+    provider = upsert_provider(provider_dict)
+    if provider is None:
+        raise HTTPException(status_code=500, detail="Provider error")
+
+    return {"provider": True}, 200
 
 @provs.put("/{id}")
 def update_prov(id: int):
