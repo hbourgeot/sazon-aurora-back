2d6e12024421e0bdb14f297f4d87b08e2c8a50cd
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-01-20 21:42:59 -0400

    adding more features to providers

--- app/api/providers.py
+++ app/api/providers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas import Provider
-from app.supabase.functions.providers import upsert_provider, get_providers
+from app.supabase.functions.providers import upsert_provider, get_providers, get_provider_by_id
 
 provs = APIRouter()
 
@@ -28,6 +28,17 @@ def create_prov(prov: Provider):
 
     return {"provider": True}, 200
 
-@provs.put("/{id}")
-def update_prov(id: int):
-    return {"message": "up"}
+@provs.put("/{prov_id}")
+def update_prov(prov_id: int, prov: Provider):
+    provider_dict = prov.model_dump()
+    
+    exists_in_db = get_provider_by_id(prov_id)
+    if exists_in_db is None:
+        raise HTTPException(status_code=404, detail="Provider not found")
+    
+    provider_dict["id"] = prov_id
+    prov_response = upsert_provider(provider_dict)
+    if prov_response is None:
+        raise HTTPException(status_code=500, detail="Provider error")
+    
+    return {"provider": True}, 200
