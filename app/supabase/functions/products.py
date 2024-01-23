f8631249c53b8129aba1118880ba1b69c269418f
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-01-23 06:28:55 -0400

    changing table name

--- app/supabase/functions/products.py
+++ app/supabase/functions/products.py
@@ -7,11 +7,11 @@ def get_foods():
 
 
 def get_food_by_id(food_id: int):
-    res = supabase.from_("foods").select("*").eq("id", food_id).execute()
+    res = supabase.from_("products").select("*").eq("id", food_id).execute()
     return res.data
 
 
 def upsert_food(data):
-    res = supabase.table("foods").upsert(data).execute()
+    res = supabase.table("products").upsert(data).execute()
     return res.data
 
