6c5c1de0415ddfc5cba4e12a4eba3f0dc271eab4
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-01-23 05:55:58 -0400

    adding more supabase product functionallity

--- app/supabase/functions/products.py
+++ app/supabase/functions/products.py
@@ -2,16 +2,16 @@ from app.supabase import supabase
 
 
 def get_foods():
-    res = supabase.from_("foods").select("*").execute()
-    print(res)
+    res = supabase.from_("products").select("*").execute()
+    return res.data
 
 
 def get_food_by_id(food_id: int):
-    res = supabase.from_("foods").select("*").eq("id", food_id)
-    print(res)
+    res = supabase.from_("foods").select("*").eq("id", food_id).execute()
+    return res.data
 
 
 def upsert_food(data):
-    res = supabase.table("foods").upsert(data)
-    print(res)
+    res = supabase.table("foods").upsert(data).execute()
+    return res.data
 
