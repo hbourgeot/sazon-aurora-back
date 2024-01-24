59fbdcc5a38216763818d583aebc726ce2c64711
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-01-24 07:25:23 -0400

    changing food supafunctions

--- app/supabase/functions/foods.py
+++ app/supabase/functions/foods.py
@@ -3,21 +3,18 @@ from app.supabase import supabase
 
 def get_foods():
     res = supabase.from_("foods").select("*").execute()
-    print(res)
-    return res
+    return res.data
 
 
 def get_food_by_id(food_id: int):
-    res = supabase.from_("foods").select("*").eq("id", food_id)
-    print(res)
-    return res
+    res = supabase.from_("foods").select("*").eq("id", food_id).execute()
+    return res.data
 
 
 def upsert_food(data):
     try:
         res = supabase.table("foods").upsert(data).execute()
-        print(res)
-        return res
+        return res.data
     except Exception as ex:
         raise ex
 
